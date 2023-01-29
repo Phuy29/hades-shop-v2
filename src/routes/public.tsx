3495552374bf5a3e30b14333e1557547f4e65b20
Phuy <phuchuy292003@gmail.com>
2023-01-29 21:58:48 +0700

    Rename files in page folder

--- src/routes/public.tsx
+++ src/routes/public.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = React.lazy(() => import('pages/home'));
-const Product = React.lazy(() => import('pages/product'));
-const Collection = React.lazy(() => import('pages/collection'));
-const Cart = React.lazy(() => import('pages/cart'));
-const Login = React.lazy(() => import('pages/Login'));
-const Register = React.lazy(() => import('pages/Register'));
+const Home = React.lazy(() => import('pages/home/Home'));
+const Product = React.lazy(() => import('pages/product/Product'));
+const Collection = React.lazy(() => import('pages/collection/Collection'));
+const Cart = React.lazy(() => import('pages/cart/Cart'));
+const Login = React.lazy(() => import('pages/login/Login'));
+const Register = React.lazy(() => import('pages/register/Register'));
 
 interface IPublicRoute {
   path: string;
